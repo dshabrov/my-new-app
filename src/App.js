627a610b2ca8e887826db1f1430ba226b69d0baa
dshabrov <dshabrov@gmail.com>
2020-09-08 22:57:13 +0200

    change content

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>хуе мое</div>
+        <div>Hue Moe</div>
       </header>
     </div>
   );

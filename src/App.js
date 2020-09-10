c04d001f6c28ee9c2a7a53e994bc65399d0a6404
dshabrov <dshabrov@gmail.com>
2020-09-10 21:38:20 +0200

    added logic and styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,107 @@
-import React, { useEffect} from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+//"https://api.exchangeratesapi.io/latest?base=USD&symbols=RUB"
+
+const fetchUtils = {
+  get: async (url, params) => {
+    try {
+      const response = await fetch(url, params);
+      if (response.status === 200) {
+        return response.json();
+      }
+      throw Error("Could not retreive the data");
+    } catch (err) {
+      return {};
+    }
+  },
+};
+
+const RateItem = (rateData) => {
+  return (
+    <div className="rate-item">
+      <span>{rateData.currency}</span>
+      <span>{rateData.rate}</span>
+    </div>
+  );
+};
+
+const CurrenciesSelect = ({ currencies = [], value, onChange }) => {
+  return (
+    <select value={value} onChange={onChange}>
+      {currencies.map((currency, idx) => (
+        <option key={idx} value={currency}>
+          {currency}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const getRates = (base, setData) => {};
 
 function App() {
-  useEffect(   () => {
-    console.log('hey im effect');
+  const [data, setData] = useState({});
+  const [base, setBase] = useState("USD");
+  const [error, setError] = useState("");
+
+  const onBaseChange = (event) => {
+    const baseValue = event.target.value;
+    setBase(baseValue.toUpperCase());
+  };
+
+  useEffect(() => {
+    console.log("did mount 1");
   });
+
+  useEffect(() => {
+    if (base.length === 3) {
+      fetchUtils
+        .get(`https://api.exchangeratesapi.io/latest?base=${base}`) //&symbols=RUB
+        .then((data) => {
+          setData(data);
+        });
+    } else {
+      setData({});
+    }
+
+    console.log("did mount 2");
+  }, [base]);
+
+  const rates = data.rates;
+  const availableCurrensies = rates ? Object.keys(rates).sort() : [];
+
+  console.log("RENDER");
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Hue Moe</div>
-      </header>
+      <div className="App-container">
+        <header>Exchange Rates</header>
+        <div className="filters-container">
+          <label>Base Currency</label>
+          {/* <input value={base} type="text" onChange={onBaseChange} />
+          <input
+            type="date"
+            onChange={(event) => {
+              console.log(event.target.value);
+            }}
+          /> */}
+          <CurrenciesSelect
+            currencies={availableCurrensies}
+            value={base}
+            onChange={onBaseChange}
+          />
+        </div>
+        {rates ? (
+          <div className="rates-container">
+            {Object.keys(rates).map((currency, idx) => (
+              <RateItem key={idx} currency={currency} rate={rates[currency]} />
+            ))}
+          </div>
+        ) : (
+          <div>No data to display</div>
+        )}
+      </div>
     </div>
   );
 }
